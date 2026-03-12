5b7047c645e01aac89b66a6b181d891e5fea737a
Daria Lasecka <daria.lasecka@htdevelopers.com>
2026-03-12 16:23:04 +0100

    Fix issue with migrations not working

---
+++ GameStore - extended/GameStore.Api/Data/Migrations/20260312151747_AddPublishersAndUpdateGamesToHaveMultipleGenres.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GameStore.Api.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPublishersAndUpdateGamesToHaveMultipleGenres : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Games_Genres_GenreId",
+                table: "Games");
+
+            migrationBuilder.RenameColumn(
+                name: "GenreId",
+                table: "Games",
+                newName: "PublisherId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Games_GenreId",
+                table: "Games",
+                newName: "IX_Games_PublisherId");
+
+            migrationBuilder.CreateTable(
+                name: "GameGenres",
+                columns: table => new
+                {
+                    GameId = table.Column<int>(type: "INTEGER", nullable: false),
+                    GenreId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GameGenres", x => new { x.GameId, x.GenreId });
+                    table.ForeignKey(
+                        name: "FK_GameGenres_Games_GameId",
+                        column: x => x.GameId,
+                        principalTable: "Games",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_GameGenres_Genres_GenreId",
+                        column: x => x.GenreId,
+                        principalTable: "Genres",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Publishers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Publishers", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GameGenres_GenreId",
+                table: "GameGenres",
+                column: "GenreId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Games_Publishers_PublisherId",
+                table: "Games",
+                column: "PublisherId",
+                principalTable: "Publishers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Games_Publishers_PublisherId",
+                table: "Games");
+
+            migrationBuilder.DropTable(
+                name: "GameGenres");
+
+            migrationBuilder.DropTable(
+                name: "Publishers");
+
+            migrationBuilder.RenameColumn(
+                name: "PublisherId",
+                table: "Games",
+                newName: "GenreId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Games_PublisherId",
+                table: "Games",
+                newName: "IX_Games_GenreId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Games_Genres_GenreId",
+                table: "Games",
+                column: "GenreId",
+                principalTable: "Genres",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
